da95965f17d1647036c386071cd649b18537a669
aleksandar-vukpavlovicpartner <Aleksandar-Vuk.Pavlovic@partner.bmw.de>
2019-01-03 15:48:32 +0100

    Refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
+// C++ includes
+#include <chrono>
+#include <thread>
 // C includes
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <time.h>
-// FreeRTOS includes
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <cstddef>
+#include <ctime>
 // ESP system includes
 #include "esp_system.h"
 // Logging includes
@@ -28,15 +26,20 @@
 #include "a2dp_core.hpp"
 #include "a2dp_cb.hpp"
 
-static const char *const SERVER_TAG = "SERVER";
+using namespace std::literals;
 
-void conjure_rms(void *_)
+namespace
 {
-	uint32_t counter = 0;
+	constexpr auto TAG = "SERVER";
+}
+
+void conjure_rms()
+{
+	std::uint32_t counter = 0;
 
 	for (;;)
 	{
-		const TickType_t start_ticks = xTaskGetTickCount();
+		const auto start = std::chrono::steady_clock::now();
 
 		if (counter % 1500 == 0)
 		{
@@ -45,81 +48,40 @@ void conjure_rms(void *_)
 
 			if (gatts::rms_value > 2 && gatts::ble_connected)
 			{
-				esp_err_t ret = esp_ble_gatts_send_indicate(
-					gatts::profile.gatts_if,
+				const auto ret = esp_ble_gatts_send_indicate(
+					gatts::interface,
 					gatts::conn_id,
 					0x2a,
-					sizeof(uint8_t),
+					sizeof(std::uint8_t),
 					&gatts::rms_value,
 					false);
 				if (ret != ESP_OK)
-					ESP_LOGE(SERVER_TAG, "Cannot notify");
+					ESP_LOGE(TAG, "Cannot notify");
 			}
 		}
 
 		counter++;
 
-		const TickType_t end_ticks = xTaskGetTickCount();
-		int ms = (end_ticks - start_ticks) * portTICK_PERIOD_MS;
-		if (ms < 16)
-			vTaskDelay((16 - ms) / portTICK_PERIOD_MS);
+		const auto end = std::chrono::steady_clock::now();
+		const auto diff = end - start;
+		if (diff < 16ms)
+            std::this_thread::sleep_for(16ms - diff);
 	}
 }
 
 
 extern "C" void app_main()
 {
-    esp_err_t ret;
-
-    /* Initialize NVS. */
-    if (nvs_flash_init() == ESP_ERR_NVS_NO_FREE_PAGES)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ESP_ERROR_CHECK(nvs_flash_init());
-    }
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ESP_ERROR_CHECK(nvs_flash_init());
 
     srand(time(NULL));
 
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"%s enable controller failed: %s",
-			__func__,
-			esp_err_to_name(ret));
-        return;
-    }
-
-    if ((ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"%s enable controller failed: %s",
-			__func__,
-			esp_err_to_name(ret));
-        return;
-    }
-
-    if ((ret = esp_bluedroid_init()) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"%s init bluetooth failed: %s",
-			__func__,
-			esp_err_to_name(ret));
-        return;
-    }
-
-    if ((ret = esp_bluedroid_enable()) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"%s enable bluetooth failed: %s",
-			__func__,
-			esp_err_to_name(ret));
-        return;
-    }
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BTDM));
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
 
     a2dp_core::start();
     a2dp_core::dispatch(
@@ -128,46 +90,10 @@ extern "C" void app_main()
 		nullptr,
 		0);
 
-    if ((ret = esp_ble_gatts_register_callback(gatts::gatts_event_handler)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"gatts register error, error code = %x",
-			ret);
-        return;
-    }
+    ESP_ERROR_CHECK(esp_ble_gatts_register_callback(gatts::event_handler));
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gatts::gap_event_handler));
+    ESP_ERROR_CHECK(esp_ble_gatts_app_register(gatts::ESP_APP_ID));
+    ESP_ERROR_CHECK(esp_ble_gatt_set_local_mtu(500));
 
-    if ((ret = esp_ble_gap_register_callback(gatts::gap_event_handler)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"gap register error, error code = %x",
-			ret);
-        return;
-    }
-
-    if ((ret = esp_ble_gatts_app_register(gatts::ESP_APP_ID)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"gatts app register error, error code = %x",
-			ret);
-        return;
-    }
-
-    if ((ret = esp_ble_gatt_set_local_mtu(500)) != ESP_OK)
-    {
-        ESP_LOGE(
-        	SERVER_TAG,
-			"set local  MTU failed, error code = %x",
-			ret);
-    }
-
-    xTaskCreate(
-    	conjure_rms,
-		"conjurer",
-		configMINIMAL_STACK_SIZE * 10,
-		NULL,
-		configMAX_PRIORITIES - 1,
-		NULL);
+    std::thread(conjure_rms).detach();
 }
